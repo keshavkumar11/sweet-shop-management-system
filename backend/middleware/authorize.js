fb49c3059b6fe4e20cbb48bef0abe37d7e68d387
keshavkumar11 <keshavkumarpurani@gmail.com>
2025-07-27 04:45:43 +0530

    refactor: use reusable authorizeRoles middleware

--- backend/middleware/authorize.js
+++ backend/middleware/authorize.js
@@ -1,6 +1,8 @@
-exports.onlyAdmin = (req,res,next)=>{
-    if (req.user && req.user.role === "admin") {
+exports.authorizeRoles = (...allowedRoles) => {
+return (req,res,next)=>{
+    if (req.user && allowedRoles.includes(req.user.role)) {
         return next();
     }
     return res.status(403).json({message:"Access Denied"});
-}
+};
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
-const {onlyAdmin} = require("../middleware/authorize");
+const {onlyAdmin, authorizeRoles} = require("../middleware/authorize");
 const{verifyToken} = require("../middleware/authMiddleware")
 
-router.post("/",verifyToken,onlyAdmin,productController.addProduct);
+router.post("/",verifyToken,authorizeRoles("admin"),productController.addProduct);
 
 module.exports = router;
